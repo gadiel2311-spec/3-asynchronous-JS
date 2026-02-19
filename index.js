98b8f444d4079cef994bc8aa9673dcc1a87c2958
gadiel2311-spec <gadiel2311@gmail.com>
2026-02-19 13:22:41 -0500

    Add new feature

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
-      console.log(res.body);
+      console.log(res.body.message);
+
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        console.log('Rondom dog image saved to file');
+      });
     });
 });
