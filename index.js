9f076925ddbe8f09621e7fc26bbd54d8aefbef24
gadiel2311-spec <gadiel2311@gmail.com>
2026-02-19 13:11:13 -0500

    Improve file handling

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const fs = require("fs");
-const superagent = require("superagent");
+const fs = require('fs');
+const superagent = require('superagent');
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
-  superagent.get(`https://dog.ceo/api/breed/hound/images/random`);
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      console.log(res.body);
+    });
 });
