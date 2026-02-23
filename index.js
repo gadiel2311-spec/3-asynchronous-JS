9b2959d9c7d7989c518e7106d27f68c7f07d6e73
gadiel2311-spec <gadiel2311@gmail.com>
2026-02-23 12:01:26 -0500

    Refactor code structure

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const superagent = require('superagent');
 const readFilePro = (file) => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 😒');
       resolve(data);
     });
   });
@@ -15,13 +16,15 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
+    .then((res) => {
       console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
         if (err) return console.log(err.message);
         console.log('Rondom dog image saved to file');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
