31e833e33fd96b95547474647a267472f6c915bc
gadiel2311-spec <gadiel2311@gmail.com>
2026-02-26 09:05:31 -0500

    update

--- index.js
+++ index.js
@@ -10,20 +10,27 @@ const readFilePro = (file) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`).then((data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        if (err) return console.log(err.message);
-        console.log('Rondom dog image saved to file');
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file 😒');
+      resolve('success');
     });
-});
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Randon dog image saved to file!');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
