e855b4ded1e90a589a7085cbe154ce86a9a17990
gadiel2311-spec <gadiel2311@gmail.com>
2026-02-26 13:03:17 -0500

    Add new feature

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const readFilePro = (file) => {
 const writeFilePro = (file, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(file, data, (err) => {
-      if (err) reject('Could not write file 😒');
+      if (err) reject(' Could not write file 😒');
       resolve('success');
     });
   });
@@ -24,23 +24,51 @@ const getDogPic = async () => {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    const res = await superagent.get(
+    const res1pro = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`,
     );
-    console.log(res.body.message);
+    const res2pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`,
+    );
+    const res3pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`,
+    );
+    const all = await Promise.all([res1pro, res2pro, res3pro]);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
+
     await writeFilePro('dog-img.txt', res.body.message);
     console.log('Randon dog image saved to file!');
   } catch (err) {
     console.log(err);
+
+    throw err;
   }
   return '2: READY 🐕';
 };
-console.log('1: Will get dog pics!');
-getDogPic().then((x) => {
-  console.log(x);
-  console.log('3: Done getting dog pics!');
-});
 
+(async () => {
+  try {
+    console.log('1: Will get dog pics!');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3; Done getting dog pics!');
+  } catch (err) {
+    console.log('ERROR 😮');
+  }
+})();
+
+/*
+console.log('1: Will get dog pics!');
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log('3: Done getting dog pics!');
+  })
+  .catch((err) => {
+    console.log('ERROR');
+  });
+*/
 /*
 readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
